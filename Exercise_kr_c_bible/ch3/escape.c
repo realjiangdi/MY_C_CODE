53c3eb846222c8d7a4b321faa241bb569495c8eb
jiangdi <jiangdi_xj@163.com>
2021-05-17 15:36:06 +0800

    update

--- Exercise_kr_c_bible/ch3/escape.c
+++ Exercise_kr_c_bible/ch3/escape.c
@@ -4,39 +4,46 @@
 #define BUF_LEN 128
 int escape(char *s, char *t)
 {
+	int ch;
+	//printf("enter escape\n");
+	while (*t != '\0')
+	{
+		//printf("entet while");
 
-    while(*t != '\0'){
-        switch (*t)
-        {
-        case '\n':
-            /* code */
-            *s++ = '\\';
-            *s++ ='n';
-            break;
-        case '\t':
-            *s++ = '\\';
-            *s++ ='t';
-            break;
-        default:
-            *s++=*t++;
-            break;
-        }
-    }
-    *s = '\0';
-    return 0;
+		switch (*t)
+		{
+		case '\n':
+			//printf("\\n\n");
+			*s++ = '\\';
+			*s++ ='n';
+			break;
+		case '\t':
+			//printf("\\t\n");
+			*s++ ='\\';
+			*s++ = 't';
+			break;
+		default:
+			*s++ = *t;
+			//printf("otherwords\n");
+			break;
+		}
+		t++;
+	}
+
+	return 0;
 }
 int main(int argc, char *argv[])
 {
 
-    char buf1[BUF_LEN] = "Hello\tWorld\nHello\tJiangDi\nHello\tCProgram\n";
-    char buf2[BUF_LEN] = {0};
+	char buf1[BUF_LEN] = "Hello\tWorld\nHello\tJiangDi\nHello\tCProgram\n";
+	char buf2[BUF_LEN] = {0};
 
-    printf("Before Convert\n");
-    printf("%s",buf1);
-    escape(buf2,buf1);
+	printf("Before Convert\n");
+	printf("%s", buf1);
+	escape(buf2, buf1);
 
-    printf("After Convert:\n");
-    printf("%s\n",buf2);
+	printf("After Convert:\n");
+	printf("%s\n",buf2);
 
-    return 0;
+	return 0;
 }
---
+++ Exercise_kr_c_bible/ch3/expands.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUF_LEN 128
+
+static int expands1(char *src, char *dst)
+{
+    int ch;
+    while (*src != '\0')
+    {
+        if (*src >= 'a' && *src <= 'z')
+        {
+            ch = *src;
+            *dst = *src++;
+            if (*src == '-')
+            {
+                src++;
+                for (; *dst != *src;)
+                {
+                    dst++;
+                    ch++;
+                    *dst = ch;
+                }
+            }
+        }
+
+        if (*src >= '0' && *src <= '9')
+        {
+            ch = *src;
+            *dst = *src++;
+            if (*src == '-')
+            {
+                src++;
+                for (; *dst != *src;)
+                {
+                    dst++;
+                    ch++;
+                    *dst = ch;
+                }
+            }
+        }
+
+        *dst++ = *src++;
+    }
+
+    *dst = '\0';
+
+    return 0;
+}
+
+static int expands(char *src, char *dst)
+{
+    int ch ;
+    while( *src != '\0'){
+        if(*src >= 'a' && *src <= 'z'){
+            ch = *src;
+            *dst = ch;
+            while(*(src +1) == '-'){
+                src+=2;
+                while(*dst != *src){
+                    dst++;
+                    ch++;
+                    *dst =ch;
+                }
+
+            }
+
+        }
+
+    }
+
+    return 0;
+}
+int main(int argc, char **argv)
+{
+    char acTestBuf1[] = {"a-z"};
+    char acTestBuf2[] = {"a-b-c"};
+    char acTestBuf3[] = {"a-z0-9"};
+    char acTestBuf4[] = {"-a-z"};
+
+    char str[BUF_LEN] = {0};
+
+    printf("expand testbuf1:%s\n", acTestBuf1);
+    expands(acTestBuf1, str);
+    printf("After expand is :%s\n", str);
+
+    // memset(str, 0, sizeof(str));
+    // printf("expand testbuf2:%s\n", acTestBuf2);
+    // expands(acTestBuf2, str);
+    // printf("After expand is :%s\n", str);
+
+    // memset(str, 0, sizeof(str));
+    // printf("expand testbuf3:%s\n", acTestBuf3);
+    // expands(acTestBuf3, str);
+    // printf("After expand is :%s\n", str);
+
+    // memset(str, 0, sizeof(str));
+    // printf("expand testbuf4:%s\n", acTestBuf4);
+    // expands(acTestBuf4, str);
+    // printf("After expand is :%s\n", str);
+
+    return 0;
+}
